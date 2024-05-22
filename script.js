9939a29cb80f9a20acded7f89c88f6c6db721848
youngeun0407 <167051137+youngeun0407@users.noreply.github.com>
2024-05-22 20:43:08 +0900

    과제 완료

--- script.js
+++ script.js
@@ -1,82 +1,30 @@
-const users = [
-  {
-    이름: "철수",
-    나이: 19,
-    성별: "남",
-  },
-  {
-    이름: "짱구",
-    나이: 20,
-    성별: "남",
-  },
-  {
-    이름: "유리",
-    나이: 21,
-    성별: "여",
-  },
-];
-console.log(users);
-
-const userContainer = document.querySelector(".user-container");
-const names = document.querySelectorAll(".name");
-const ages = document.querySelectorAll(".age");
-const genders = document.querySelectorAll(".gender");
-
-users.forEach(function (users) {
-  let html템플릿 = `<div class="user-container">
-										<h3 class="name">이름: ${users.이름}</h3>
-										<h3 class="age">나이: ${users.나이}</h3>
-                    <h3 class="gender">성별: ${users.성별}</h3>
-									</div>`;
-  userContainer.insertAdjacentHTML("beforeend", html템플릿);
-});
-
-//3번
-const girl = users.find((user) => {
- console.log(user);
-  return (user.성별 === "여"); 
-});
-console.log(girl);
-
-//4번
-const boy = users.filter((user) => {
-  return (user.성별 === "남");
-});
-
-console.log(boy);
-//위의 문제와 동일하게 콘솔에 모두 "남"으로 표기됨
-
-//5번
-// const newAge = users.map(user => {
-//   return {
-//   ...user,
-//   나이: user.나이 + 10
-//   }
-//   })
-
-const newAge = users.map((user) => {
-  return {
-    이름: user.이름,
-    나이: user.나이 + 10,
-    성별: user.성별,
-  };
-});
-console.log(newAge); //콘솔 모든 정보가 철수의 정보로 출력
-console.log(users); //1번과 동일한 결과 유리가 "남"으로 출력
-
-//6번
-let a = { ...users };
-console.log(a);
-users.sort((a, b) => {
-  return b.나이 - a.나이;
-});
-console.log(users);
 
 //7번
 const user = {
   이름: "병수",
   나이: 20,
-  주소: "시골",
+  주소: "시골"
 };
-const { 이름, 나이, 주소 } = user;
-console.log(user);
+
+// 1. 객체를 JSON 문자열로 변환하여 로컬스토리지에 저장
+localStorage.setItem('user', JSON.stringify(user));
+
+// 2. 로컬스토리지에서 데이터를 조회하고 객체로 변환
+const storedUser = localStorage.getItem('user');
+const userObject = JSON.parse(storedUser);
+console.log(userObject);
+
+// 3. 나이를 30으로 변경 후 다시 로컬스토리지에 저장
+userObject.나이 = 30;
+localStorage.setItem('user', JSON.stringify(userObject));
+
+// 4. 로컬스토리지에서 user 데이터를 삭제하고 조회
+localStorage.removeItem('user');
+const deletedUser = localStorage.getItem('user');
+console.log(deletedUser); // null
+
+
+//메소드:get 생략가능! 
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
